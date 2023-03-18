9b29773f64c2eed7876163ee60f798d24c01faa7
“elvisndungu” <senkutoshi@gmail.com>
2023-03-17 19:14:30 -0700

    new commit to C!!

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all alphabets in uppercase then lowercase
+ * Return: 0 if all GG
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch != 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
