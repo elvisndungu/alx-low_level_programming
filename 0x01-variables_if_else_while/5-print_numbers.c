6372f35bb74279e2dadf7ad00d0d5ac34659d33b
“elvisndungu” <senkutoshi@gmail.com>
2023-03-17 19:28:45 -0700

    new commit to C!!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print alphabets in upper case and lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 10; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
