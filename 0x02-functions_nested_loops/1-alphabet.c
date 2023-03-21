1c85b32bc3326715446da7496dcb1624dd15f154
“elvisndungu” <senkutoshi@gmail.com>
2023-03-21 09:07:49 -0700

    New commit🥹 to functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+
+	_putchar('\n');
+}
