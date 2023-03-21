3e6cc6c8def77b587991e34f91776cc4ca7e7058
“elvisndungu” <senkutoshi@gmail.com>
2023-03-21 16:05:00 -0700

    New commit🥹 to functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ *
+ * @one: first number input
+ * @two: second munber input
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - a function that prints a 11 natural numbers from n to 98
+ * @n: number input
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
