dbab779296912db2f8c0512ada830cc7121c7cfd
“elvisndungu” <senkutoshi@gmail.com>
2023-03-27 00:42:25 -0700

    🌚new task commit to pointers🗿 and stringss🥹

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter, updates it to 98
+ * @n: pointer n
+ *
+ * Return: void .... nada
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
