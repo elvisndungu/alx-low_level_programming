2757d14479b21762b769b19b87ed46094d149f09
elvisndungu <senkutoshi@gmail.com>
2023-04-04 04:50:43 +0200

    repatch

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- *_strstr-locates a substring
- *@haystack:string to check occurence of needle
- *@needle:string to be checked.
- *Return:Returns a pointer to the beginning of the located
- * substring, or NULL if the substring is not found.
+ * _strstr - locates a substring
+ * @haystack: str to check occurence of needle
+ * @needle: str  to be checked.
+ * Return: pointer to the beginning of the located
  */
 
 char *_strstr(char *haystack, char *needle)
