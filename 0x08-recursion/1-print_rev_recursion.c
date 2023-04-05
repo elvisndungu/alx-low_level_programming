4e68174627901c494615039b938954cc0aa50045
elvisndungu <senkutoshi@gmail.com>
2023-04-05 11:32:00 -0700

    🔂new task commit to recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+/**
+ * _print_rev_recursion - prints a str in reversee
+ * @s: the address of memory to print
+ *
+ * Return: notin at all :)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	/* if we're at the end of a string terminate */
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s+1); 
+	/* 
+	 * when we reach the end  of str it will
+	 * return notin and allow the following code to run
+	 */
+	_putchar(*s);
+}
