c31195a11c79e3ddfb5cf95d5d150183defa161c
elvisndungu <senkutoshi@gmail.com>
2023-04-06 01:58:55 +0200

    WHAT DA HELL IS THIS TASK

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,37 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the square root of a number
- * @n: int
- * @i: int
- * Return: anoda int
+ * powaa - returns the natural square root of a number
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
  */
 
-int _sqrt_recursion(int n, int i)
+int powaa(int n, int c)
 {
-/* If n is negative, return -1, no cap */
-if (n < 0)
-return (-1);
-
-/* Base case: if n is 0 or 1, return n straight up */
-if (n == 0 || n == 1)
-return (n);
-
-/* If i*i is equal to n, return i - got that sqrt, homie! */
-if (i * i == n)
-return (i);
-
-/* If i*i is less than n, increment i and try again - keep it pushin */
-if (i * i < n)
-return (_sqrt_recursion(n, i + 1));
-
-/* If i*i is greater than n, return -1 - sorry bro, can't get that sqrt */
-return (-1);
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+		return (0 + powaa(n, c + 1));
 }
+
 /**
- * _sqrt_recursion - returns the square root of a number
- * @n: integer
- * Return: integer
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root
  */
 
 int _sqrt_recursion(int n)
 {
-/* Let's get that sqrt :) */
-return (_sqrt_recursion(n, 0));
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (powaa(n, 2));
 }
