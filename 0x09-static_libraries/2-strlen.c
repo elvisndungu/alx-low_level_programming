3de9296bc975b5ff18b62235adc8007b17712267
elvisndungu <senkutoshi@gmail.com>
2023-04-10 07:45:30 -0700

    Final p

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen  - prints len of pointer s
+ * @s: pointer to string s
+ * Return: integer .... len of te string
+ */
+
+int _strlen(char *s)
+{
+	char *string = s;
+	int len = strlen(string);
+
+	return (len);
+}
