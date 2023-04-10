abdcbad9080fbea3d92f6aedc60d065e289b5017
elvisndungu <senkutoshi@gmail.com>
2023-04-10 13:54:24 +0200

    omg source files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string followed by new line
+ * @str: pointer a
+ * Return: void .... nada
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
