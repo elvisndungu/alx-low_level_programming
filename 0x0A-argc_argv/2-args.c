1ac497c77ad1ca40533fbe15a27ac31e6fdd71ca
elvisndungu <senkutoshi@gmail.com>
2023-04-10 18:08:23 -0700

    ezzii

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main  - prints all arguments it recieves
+ * @argc: gets count of arguments
+ * @argv: array of cmd line arguments
+ * Return: 0 nada
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
