dfa70f3a6d905f6fd7ee61bf82987482e7599449
elvisndungu <senkutoshi@gmail.com>
2023-04-18 05:51:53 -0700

    👅 first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+
+#endif
