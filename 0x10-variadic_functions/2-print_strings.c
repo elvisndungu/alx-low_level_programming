c79878c256c34fc8211d1bbf6c2624eefb078d9e
elvisndungu <senkutoshi@gmail.com>
2023-04-20 18:11:06 -0700

    👩‍🚀 betty oatgc

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,20 +8,20 @@
  * @n: the number of strings passed to the function
  * @...: the strings to be printed
  */
-void print_strings(const char * separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	char * str;
+	char *str;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char * );
+		str = va_arg(args, char *);
 		printf("%s", str ? str : "(nil)");
 
-		if (i != n - 1 && separator != NULL) 
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
