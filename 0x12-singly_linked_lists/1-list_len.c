4f9859e91a733032c56cf993318b74e979456deb
elvisndungu <senkutoshi@gmail.com>
2023-04-28 21:22:35 +0200

    new task commit :)

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a linked list_t list.
+ * @h: The head node
+ * Return: the number of elements in a linked
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	size_t counter = 0;
+	
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	for (temp = h; temp != NULL; temp = temp->next)
+	{
+		counter++;
+	}
+
+	return (counter);
+	
+}
