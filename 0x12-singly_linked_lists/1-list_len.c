2111061d7fba3cdce0af3628012f6289cf8efca7
elvisndungu <senkutoshi@gmail.com>
2023-04-30 21:25:17 +0200

    nice

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * list_len - function that returns the number of elements in a linked list_t list.
+ * list_len - returns the number of elements in a linked list_t list
  * @h: The head node
  * Return: the number of elements in a linked
  */
