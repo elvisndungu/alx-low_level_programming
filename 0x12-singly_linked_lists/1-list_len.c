7d49b657ebe96fde87711b03c4ff6b2401f1650b
elvisndungu <senkutoshi@gmail.com>
2023-05-01 15:00:53 +0200

    betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,7 +8,7 @@ size_t list_len(const list_t *h)
 {
 	const list_t *temp;
 	size_t counter = 0;
-	
+
 	if (h == NULL)
 	{
 		return (0);
@@ -18,7 +18,6 @@ size_t list_len(const list_t *h)
 	{
 		counter++;
 	}
-
 	return (counter);
-	
+
 }
