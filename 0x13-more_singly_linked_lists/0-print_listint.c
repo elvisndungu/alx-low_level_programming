bcd506a12be988d25562aa62c84d9971b37e7873
elvisndungu <senkutoshi@gmail.com>
2023-05-03 13:04:50 +0200

    betty cleanup

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,17 @@ size_t print_listint(const listint_t *h)
 {
 	const listint_t *temp;
 	size_t counter = 0;
-	
+
 	if (h == NULL)
 	{
 		printf("Nah uh");
 		return (0);
 	}
-	for (temp = h; temp != NULL; temp = temp->next) {
+	for (temp = h; temp != NULL; temp = temp->next)
+	{
 		printf("%i\n", temp->n);
 		counter++;
 	}
-	
+
 	return (counter);
 }
