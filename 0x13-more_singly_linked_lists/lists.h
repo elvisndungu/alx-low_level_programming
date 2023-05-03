8f1f6add4f04a49c2b9f0d40909d5c9d90a8cbda
elvisndungu <senkutoshi@gmail.com>
2023-05-03 23:29:52 +0200

    let me cook now!

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -60,7 +60,7 @@ size_t print_listint_safe(const listint_t *head);
 
 size_t free_listint_safe(listint_t **h);
 
-listint_t *find_listint_loop(listint_t *head)
+listint_t *find_listint_loop(listint_t *head);
 
 
 
